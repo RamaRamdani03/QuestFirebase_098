bbcd01eb9ea76d9295d4bc7436901bad43f70d31
RamaRamdani03 <ramaramdani449@gamil.com>
2025-01-13 04:05:16 +0700

    Menambahkan `PengelolaHalaman` untuk menangani navigasi antara `HomeScreen` dan `InsertMhsView`

---
+++ app/src/main/java/com/example/firebasepam/ui/navigation/PengelolaHalaman.kt
@@ -0,0 +1,38 @@
+package com.example.firebasepam.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.firebasepam.ui.view.HomeScreen
+import com.example.firebasepam.ui.view.InsertMhsView
+
+
+@Composable
+fun PengelolaHalaman(
+    modifier: Modifier,
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = {
+                    navController.navigate(DestinasiInsert.route)
+                },
+            )
+        }
+        composable(DestinasiInsert.route) {
+            InsertMhsView(
+                onBack = { navController.popBackStack() },
+                onNavigate = {
+                    navController.navigate(DestinasiHome.route) }
+            )
+        }
+    }
+}
