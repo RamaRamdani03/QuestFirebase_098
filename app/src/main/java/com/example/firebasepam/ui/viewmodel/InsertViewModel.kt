7ff8c259f95ca0f5eb6cc48d951baab4da4be9fd
RamaRamdani03 <ramaramdani449@gamil.com>
2025-01-13 01:12:36 +0700

    Menambahkan `MahasiswaEvent` dan fungsi konversi ke `Mahasiswa`

--- app/src/main/java/com/example/firebasepam/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/firebasepam/ui/viewmodel/InsertViewModel.kt
@@ -1,3 +1,5 @@
+import com.example.firebasepam.model.Mahasiswa
+
 sealed class FormState {
     object Idle : FormState()
     object Loading : FormState()
@@ -18,3 +20,24 @@ data class FormErrorState(
                 alamat == null && kelas == null && angkatan == null
     }
 }
+
+
+// data class variable yang menyimpan data input form
+data class MahasiswaEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val jenis_kelamin: String = "",
+    val alamat: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
+
+// Menyimpan input form ke dalam entity
+fun MahasiswaEvent.toMhsModel(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    jenis_kelamin = jenis_kelamin,
+    alamat = alamat,
+    kelas = kelas,
+    angkatan = angkatan
+)
